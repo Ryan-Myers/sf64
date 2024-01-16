77fcf3dd2e8fc6f1fab46bcd94ac846e3339be4e
Ryan Myers <ryan.p.myers@gmail.com>
2024-01-15 21:58:37 -0500

    Match func_i1_80198DCC

--- src/overlays/ovl_i1/E05990.c
+++ src/overlays/ovl_i1/E05990.c
@@ -29,48 +29,44 @@ void func_i1_80198C50(void) {
     }
 }
 
-#if 0
-s32 func_i1_80198DCC(Object_58* arg0, f32 arg1, f32 arg2) {
+s32 func_i1_80198DCC(Object_58* obj58, f32 x, f32 y) {
     Object_58* obj58;
     s32 i;
 
-    obj58 = gObjects58;
-    for (i = 0; i < 200; i++, obj58++) {
-        if ((obj58->obj.status == 2) && 
-                (fabsf(obj58->obj.pos.x - (arg0->obj.pos.x + ((arg1 * 650.0f) + 350.0f))) < 1200.0f) && 
-                (fabsf(obj58->obj.pos.z - (arg0->obj.pos.z + ((arg2 * 650.0f) + 350.0f))) < 1200.0f) && 
-                (arg0->obj.pos.y < 650.0f)) 
-        {
+    for (i = 0, obj58 = gObjects58; i < 200;) {
+        if ((obj58->obj.status == 2) &&
+            (fabsf(obj58->obj.pos.x - (obj58->obj.pos.x + ((x * 650.0f) + 350.0f))) < 1200.0f) &&
+            (fabsf(obj58->obj.pos.z - (obj58->obj.pos.z + ((y * 650.0f) + 350.0f))) < 1200.0f) &&
+            (obj58->obj.pos.y < 650.0f)) {
             return 1;
         }
+        obj58++;
+        if ((obj58->obj.status == 2) &&
+            (fabsf(obj58->obj.pos.x - (obj58->obj.pos.x + ((x * 650.0f) + 350.0f))) < 1200.0f) &&
+            (fabsf(obj58->obj.pos.z - (obj58->obj.pos.z + ((y * 650.0f) + 350.0f))) < 1200.0f) &&
+            (obj58->obj.pos.y < 650.0f)) {
+            return 1;
+        }
+        obj58++;
+        if ((obj58->obj.status == 2) &&
+            (fabsf(obj58->obj.pos.x - (obj58->obj.pos.x + ((x * 650.0f) + 350.0f))) < 1200.0f) &&
+            (fabsf(obj58->obj.pos.z - (obj58->obj.pos.z + ((y * 650.0f) + 350.0f))) < 1200.0f) &&
+            (obj58->obj.pos.y < 650.0f)) {
+            return 1;
+        }
+        obj58++;
+        if ((obj58->obj.status == 2) &&
+            (fabsf(obj58->obj.pos.x - (obj58->obj.pos.x + ((x * 650.0f) + 350.0f))) < 1200.0f) &&
+            (fabsf(obj58->obj.pos.z - (obj58->obj.pos.z + ((y * 650.0f) + 350.0f))) < 1200.0f) &&
+            (obj58->obj.pos.y < 650.0f)) {
+            return 1;
+        }
+        obj58++;
+        i += 4;
     }
-//     var_v0 = 0;
-// loop_1:
-//     if ((gObjects58[i].obj.status == 2) && (fabsf(gObjects58[i].obj.pos.x - (arg0->obj.pos.x + ((arg1 * 650.0f) + 350.0f))) < 1200.0f) && (fabsf(gObjects58[i].obj.pos.z - (arg0->obj.pos.z + ((arg2 * 650.0f) + 350.0f))) < 1200.0f) && (arg0->obj.pos.y < 650.0f)) {
-//         return 1;
-//     }
-//     temp_v1 = var_v1 + 0x58;
-//     if ((gObjects58[i].unk58 == 2) && (fabsf(temp_v1->obj.pos.x - (arg0->obj.pos.x + ((arg1 * 650.0f) + 350.0f))) < 1200.0f) && (fabsf(temp_v1->obj.pos.z - (arg0->obj.pos.z + ((arg2 * 650.0f) + 350.0f))) < 1200.0f) && (arg0->obj.pos.y < 650.0f)) {
-//         return 1;
-//     }
-//     temp_v1_2 = temp_v1 + 0x58;
-//     if ((temp_v1->unk58 == 2) && (fabsf(temp_v1_2->obj.pos.x - (arg0->obj.pos.x + ((arg1 * 650.0f) + 350.0f))) < 1200.0f) && (fabsf(temp_v1_2->obj.pos.z - (arg0->obj.pos.z + ((arg2 * 650.0f) + 350.0f))) < 1200.0f) && (arg0->obj.pos.y < 650.0f)) {
-//         return 1;
-//     }
-//     temp_v1_3 = temp_v1_2 + 0x58;
-//     var_v0 += 4;
-//     if ((temp_v1_2->unk58 == 2) && (fabsf(temp_v1_3->obj.pos.x - (arg0->obj.pos.x + ((arg1 * 650.0f) + 350.0f))) < 1200.0f) && (fabsf(temp_v1_3->obj.pos.z - (arg0->obj.pos.z + ((arg2 * 650.0f) + 350.0f))) < 1200.0f) && (arg0->obj.pos.y < 650.0f)) {
-//         return 1;
-//     }
-//     var_v1 = temp_v1_3 + 0x58;
-//     if (var_v0 == 0xC8) {
-//         return 0;
-//     }
-//     goto loop_1;
+
+    return 0;
 }
-#else
-#pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i1/E05990/func_i1_80198DCC.s")
-#endif
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/overlays/ovl_i1/E05990/func_i1_80199024.s")
 
